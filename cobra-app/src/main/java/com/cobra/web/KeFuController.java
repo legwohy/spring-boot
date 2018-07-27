da1447a5ad2ac23851408525eb029cd561fbb0c2
leigang <lg@iyingke.cn>
2018-07-27 19:27:37 +0800

    消息参数原样返回

--- cobra-app/src/main/java/com/cobra/web/KeFuController.java
+++ cobra-app/src/main/java/com/cobra/web/KeFuController.java
@@ -1,16 +1,20 @@
 package com.cobra.web;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.cobra.constants.QiyuConstant;
 import com.cobra.constants.TokenConstant;
 import com.cobra.dto.QiyuMessageDto;
 import com.cobra.param.BaseResponse;
 import com.cobra.util.*;
 import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @Api("客服系统")
 @RestController
 @RequestMapping("/api/kefu")
@@ -56,7 +60,8 @@ public class KeFuController
     @RequestMapping("/sendMessage")
     public BaseResponse sendMessage(@RequestBody QiyuMessageDto qiyuMessageDto){
 
-      String content = qiyuMessageDto.toString();
+      String content = JSON.toJSONString(qiyuMessageDto);
+      log.info("-------->"+content);
       long time = DateUtils.getUTCTime();
 
         String url = QiyuConstant.URL_SEND_MESSAGE
@@ -64,8 +69,12 @@ public class KeFuController
                 .concat("&time="+ time)
                 .concat("&checksum="+ QiyuPushCheckSum.encode(QiyuConstant.QIYU_APP_SECRET, MD5.md5(content),time));
         String rs = OkHttpUtil.post(url,content);
+        JSONObject jsonObject = JSON.parseObject(rs);
+        if(!"200".equals(jsonObject.getString("code"))){
+            return ResponseUtil.error();
+        }
 
-        return ResponseUtil.success(rs);
+        return ResponseUtil.success(content);
 
     }
 }
