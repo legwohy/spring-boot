c2db167325f8be997ef3d4f49028812914e2e3d3
leigang <lg@iyingke.cn>
2018-12-21 16:49:51 +0800

    oss 批量上传文件

--- cobra-common/src/main/java/com/cobra/util/oss/MainClass.java
+++ cobra-common/src/main/java/com/cobra/util/oss/MainClass.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.UUID;
 
 /**
@@ -16,14 +17,56 @@ import java.util.UUID;
 public class MainClass
 {
     public final static long expire = 1000 * 3600 * 24 * 15L;
-    public static void main(String[] args) throws FileNotFoundException
+    public static void main(String[] args) throws Exception
     {
-        String region = "hangzhou";// 区域
-        String keyId = "";
-        String secret = "";
+
+        // 存储空间名
+        String bucketName = "jinlihua-bank-logo";
+        String ossKey = "files/bank/";
+
+        String filePath = "F:\\项目\\保险\\银行缩图\\";
+
+
+        File file = new File(filePath);
+        if(file.isDirectory())
+        {
+            String[] fileNameList = file.list();
+            Arrays.stream(fileNameList).forEach(fileName->{
+
+                    if(fileName.contains(".png"))
+                    {
+                        try
+                        {
+                            File file1 = new File(file.getAbsolutePath()+File.separator+fileName);
+                            FileInputStream inputStream = new FileInputStream(file1);
+                            getOSSUpload().sampleUploadWithInputStream(bucketName,ossKey+fileName,inputStream);
+
+                            System.out.println(fileName+"上传成功");
+
+                        }
+                        catch (FileNotFoundException e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+
+            });
+
+
+
+
+        }
+
+
+    }
+
+    public static OSSUpload getOSSUpload(){
+        String region = "shanghai";// 区域
+        String keyId = "LTAIi1kpSZhwlene";
+        String secret = "8RcUl9BMahCRjLglRhwpt6Whi80rfg";
         String bucketAcl = "public-read";// 读写权限
         String bucketStorage = "standard";// 存储配置
-        String bucketName = "banner-test";// 存储空间名
+
         OSSClient ossClient = new OSSClient("http://oss-cn-" + region + ".aliyuncs.com", keyId, secret);
         OSSOperation ossOperation = new OSSOperation(ossClient);
         ossOperation.setBucketAcl(bucketAcl);
@@ -31,16 +74,6 @@ public class MainClass
 
         OSSUpload ossUpload = new OSSUpload(ossOperation);
 
-        String ossKey = "file/sb/test/"+ UUID.randomUUID().toString()+".png";
-        File file = new File("F:\\0 手持.png");
-        FileInputStream inputStream = new FileInputStream(file);
-
-        // 上传
-        OSSModel model = ossUpload.sampleUploadWithInputStream(bucketName,ossKey,inputStream);
-
-
-        String url = ossUpload.sampleGetFileUrl(bucketName, ossKey, expire).toString();
-        System.out.println("url="+url);
-
+        return ossUpload;
     }
 }
