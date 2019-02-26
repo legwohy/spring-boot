7379f42ed15a5c22f4bde509b4338d2bfc88eeba
leigang <lg@iyingke.cn>
2019-02-26 14:42:57 +0800

    google规范 import static 引入静态方法 不能使用*

---
+++ cobra-learning/src/main/java/com/cobra/code/importStatic/MainTest.java
@@ -0,0 +1,20 @@
+package com.cobra.code.importStatic;
+
+import java.util.stream.Stream;
+
+import static com.cobra.code.importStatic.Print.print1;
+
+/**
+ * @auther: lg
+ * @date: 2019/2/26 14:40
+ * @description:
+ */
+public class MainTest
+{
+    public static void main(String[] args)
+    {
+        // import static
+        print1();
+
+    }
+}
