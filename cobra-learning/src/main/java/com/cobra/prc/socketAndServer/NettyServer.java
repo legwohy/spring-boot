bb771def7102ee62f29c1c5914b2daddad0805fb
legwohy <33128494+legwohy@users.noreply.github.com>
2019-05-14 23:36:54 +0800

    NIO与BIO

---
+++ cobra-learning/src/main/java/com/cobra/prc/socketAndServer/NettyServer.java
@@ -0,0 +1,8 @@
+package com.cobra.prc.socketAndServer;
+
+/**
+ * 也能完成NIO操作 Netty是对NIO的封装
+ */
+public class NettyServer
+{
+}
---
+++ cobra-learning/src/main/java/com/cobra/prc/socketAndServer/ServerThreadPool.java
@@ -0,0 +1,36 @@
+package com.cobra.prc.socketAndServer;
+
+import springfox.documentation.RequestHandler;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by legwo on 2019/5/14.
+ */
+public class ServerThreadPool
+{
+    ExecutorService service = Executors.newFixedThreadPool(2);
+    public static void main(String[] args)
+    {
+        ExecutorService service = Executors.newFixedThreadPool(2);// 两个线程
+        try
+        {
+            ServerSocket serverSocket = new ServerSocket(9999);
+            System.out.println("NIOServer has start,listening port "+serverSocket.getLocalSocketAddress());
+            while (true){
+                Socket socket = serverSocket.accept();
+                System.out.println("Connection from "+socket.getRemoteSocketAddress());
+
+
+
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
