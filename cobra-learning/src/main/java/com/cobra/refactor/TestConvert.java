0bbccc829756385c45ac57ada404289cfe68bc76
leigang <leigang-zx@bestpay.com.cn>
2019-09-27 12:29:33 +0800

    代码重构-类型转换

---
+++ cobra-learning/src/main/java/com/cobra/refactor/TestConvert.java
@@ -0,0 +1,47 @@
+package com.cobra.refactor;
+
+import lombok.Data;
+import org.springframework.beans.BeanUtils;
+
+/**
+ * Created by leigang on 2019/9/27.
+ */
+public class TestConvert {
+    public static void main(String[] args) {
+        AbstractDTOConvert convert = new AbstractDTOConvert<TestA,TestB>() {
+            @Override
+            protected TestB doForward(TestA testA) {
+                TestB testB = new TestB();
+                BeanUtils.copyProperties(testA,testB);
+
+                return testB;
+            }
+
+            @Override
+            protected TestA doBackward(TestB testB) {
+                TestA testA = new TestA();
+                BeanUtils.copyProperties(testB,testA);
+
+                return testA;
+            }
+        };
+
+        /** testA--->testB */
+        System.out.println("正向:"+convert.convert(new TestA()));
+
+        /**testB-->testA */
+        System.out.println("反向:"+convert.reverse().convert(new TestB()));
+
+    }
+
+
+    @Data
+    private static  class TestA{
+        private Integer id;
+    }
+    @Data
+    private static class TestB{
+        private Integer id;
+        private String name;
+    }
+}
