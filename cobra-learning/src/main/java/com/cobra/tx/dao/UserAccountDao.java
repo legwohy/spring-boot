515b931cab03da50af788c9c0ef33ef4d0464d10
legwohy <33128494+legwohy@users.noreply.github.com>
2019-12-21 10:59:00 +0800

    手写事务

---
+++ cobra-learning/src/main/java/com/cobra/tx/dao/UserAccountDao.java
@@ -0,0 +1,24 @@
+package com.cobra.tx.dao;
+
+import com.cobra.tx.frame.SingleConnectionHolder;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 每一个sql从数据源中取得连接
+ */
+public class UserAccountDao
+{
+    private DataSource dataSource;
+    public UserAccountDao(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+    public void order()throws SQLException{
+        Connection conn = SingleConnectionHolder.getConnection(dataSource);
+
+        // 业务操作
+        System.out.println("当前【用户】线程:"+Thread.currentThread().getName()+",连接通道 hashcode="+conn.hashCode());
+    }
+}
---
+++ cobra-learning/src/main/java/com/cobra/tx/frame/CobraTransactionManager.java
@@ -0,0 +1,49 @@
+package com.cobra.tx.frame;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 一个线程中的一个事务的多个操作，使用的是同一个Connection
+ */
+public class CobraTransactionManager
+{
+    private DataSource dataSource;
+    public CobraTransactionManager(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+    private Connection getConnection()throws SQLException{
+        return SingleConnectionHolder.getConnection(dataSource);
+    }
+
+    // 开启 一个事务必须是同一个连接
+    public void start()throws SQLException{
+       Connection conn =  getConnection();
+       conn.setAutoCommit(false);// 手动提交
+    }
+    // 回滚
+    public void rollBack(){
+        Connection conn = null;
+        try
+        {
+            conn = getConnection();
+            conn.rollback();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+    // 关闭
+    public void close()throws SQLException{
+        Connection conn =  getConnection();
+        conn.setAutoCommit(false);// 手动提交
+        conn.close();
+    }
+
+    public DataSource getDataSource(){
+        return dataSource;
+    }
+}
