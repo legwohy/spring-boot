b0548e75bedba760e1e2f4e2cf55c27202ad4aba
admin <33128494+legwohy@users.noreply.github.com>
2021-01-27 21:59:07 +0800

    数组排序

--- cobra-common/src/main/java/com/cobra/util/MapUtils.java
+++ cobra-common/src/main/java/com/cobra/util/MapUtils.java
@@ -9,26 +9,43 @@ import java.util.*;
  * @date 2021/1/11 20:37
  * @desc
  */
-public class MapUtils {
+public class MapUtils
+{
     static String separation = ".";
 
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
 
-                Map<String, Object> container = new HashMap<>();
-                getMapData().forEach((key, value) -> {
-                    Map<String, Object> data = change(new HashMap<>(), key, value);
-                    merge(container, data);
-                });
+        Map<String, Object> container = new HashMap<>();
+        getMapData().forEach((key, value) ->
+        {
+            Map<String, Object> data = change(new HashMap<>(), key, value);
+            merge(container, data);
+        });
 
-                // 将toString方法覆盖,输出想要的json格式
+        // 将toString方法覆盖,输出想要的json格式
 
-                // String json = jsonStyleFormat(container);
-               // System.out.println(JSON.toJSONString(container));
+        // String json = jsonStyleFormat(container);
+        // System.out.println(JSON.toJSONString(container));
 
         String json = "";
 
         updateValue(container, "credit.header.mobile", "test");
-        System.out.println(container);
+        //System.out.println(container);
+
+        List<String> initList = new ArrayList<>();
+        initList.add("credit.header.mobile");
+        initList.add("credit.body");
+        initList.add("credit.header.version_fix");
+        initList.add("credit.header.flag_system");
+
+        initList.add("credit.mac");
+        System.out.println("原序:" + initList);
+        initList.sort(((o1, o2) -> o1.compareTo(o2)));
+        System.out.println("升序:" + initList);
+
+        initList.sort(((o2, o1) -> o1.compareTo(o2)));
+        System.out.println("降序:" + initList);
 
     }
 
@@ -37,7 +54,8 @@ public class MapUtils {
      *
      * @param container 要格式化的的数据
      */
-    private static String jsonStyleFormat(Map<String, Object> container){
+    private static String jsonStyleFormat(Map<String, Object> container)
+    {
         String mapString = container.toString();
         System.out.println(mapString);
         return mapString.replaceAll("=", ":");
@@ -50,20 +68,26 @@ public class MapUtils {
      * @param data      要遍历查看的数据
      * @return 合并之后的数据
      */
-    private static Map<String, Object> merge(Map<String, Object> container, Map<String, Object> data){
+    private static Map<String, Object> merge(Map<String, Object> container, Map<String, Object> data)
+    {
         // 遍历map中的数据
-        data.forEach((sonKey, sonValue) -> {
+        data.forEach((sonKey, sonValue) ->
+        {
             // 如果容器中包含了目前遍历的key,就代表这个key已经存在,需要覆盖处理
-            if (container.containsKey(sonKey)) {
+            if (container.containsKey(sonKey))
+            {
                 // 获取覆盖之前的数据
                 Object oldValue = container.get(sonKey);
                 // 如果不是map,就不需要再次判断了
-                if (!(sonValue instanceof Map)) {
+                if (!(sonValue instanceof Map))
+                {
                     return;
                 }
                 // 将新的合并的值放入容器
                 container.put(sonKey, merge((Map<String, Object>)sonValue, (Map<String, Object>)oldValue));
-            } else {
+            }
+            else
+            {
                 // 如果没有冲突,直接存入map
                 container.put(sonKey, sonValue);
             }
@@ -79,9 +103,11 @@ public class MapUtils {
      * @param value key对应的值
      * @return 解析之后的map
      */
-    private static Map<String, Object> change(Map<String, Object> map, String key, Object value){
+    private static Map<String, Object> change(Map<String, Object> map, String key, Object value)
+    {
         // 判断字符串是否有.号
-        if (key.contains(separation)) {
+        if (key.contains(separation))
+        {
             // 只要有.号,就代表有子map
             Map<String, Object> sonMap = new HashMap<>();
             // 获取第一个.的位置方便获取key
@@ -93,7 +119,9 @@ public class MapUtils {
             String sonKey = key.substring(beginIndex + firstKey.length() + 1);
             // 将截取的第一个字符串重新装配
             map.put(firstKey, change(sonMap, sonKey, value));
-        } else {
+        }
+        else
+        {
             // 没有点号,代表只有一个
             map.put(key, value);
         }
@@ -103,7 +131,8 @@ public class MapUtils {
     /**
      * 提供测试数据
      */
-    private static Map<String, Object> getMapData(){
+    private static Map<String, Object> getMapData()
+    {
         List<String> initList = new ArrayList<>();
         initList.add("credit.header.mobile");
         initList.add("credit.body");
@@ -111,33 +140,39 @@ public class MapUtils {
         initList.add("credit.header.flag_system");
 
         initList.add("credit.mac");
+
         Map<String, Object> map = new LinkedHashMap<>();
-        for (String key : initList) {
+        for (String key : initList)
+        {
             map.put(key, key);
         }
 
         return map;
     }
 
-    public static void updateValue(String json,String keyIndex,Object value){
-        Map map = JSON.parseObject(json,Map.class);
-        updateValue(map,keyIndex,value);
+    public static void updateValue(String json, String keyIndex, Object value)
+    {
+        Map map = JSON.parseObject(json, Map.class);
+        updateValue(map, keyIndex, value);
     }
-    private static void updateValue(Map srcMap, String keyIndex, Object value){
+
+    private static void updateValue(Map srcMap, String keyIndex, Object value)
+    {
         String[] keArr = keyIndex.split("\\.");
-        if (keArr.length == 1 || keArr.length == 0) {
+        if (keArr.length == 1 || keArr.length == 0)
+        {
             srcMap.put(keyIndex, value);
             return;
         }
         String key = keArr[0];
         Map map = (Map)srcMap.get(key);
-        if(null == map){
+        if (null == map)
+        {
             throw new RuntimeException("参数异常");
         }
         keyIndex = keyIndex.substring(keyIndex.indexOf(separation) + 1, keyIndex.length());
         updateValue(map, keyIndex, value);
     }
-
 
     /**
      * 读取json中的值
@@ -145,7 +180,8 @@ public class MapUtils {
      * @param keyIndex
      * @return
      */
-    public static Object readValue(String json, String keyIndex){
+    public static Object readValue(String json, String keyIndex)
+    {
         LinkedHashMap map = JSON.parseObject(json, LinkedHashMap.class);
         return getValue(map, keyIndex);
 
@@ -157,21 +193,29 @@ public class MapUtils {
      * @param key credit.header.errorCode
      * @return
      */
-    private static Object getValue(Map<String, Object> srcMap, String key){
-        if (!key.contains(separation)) {
+    private static Object getValue(Map<String, Object> srcMap, String key)
+    {
+        if (!key.contains(separation))
+        {
             return srcMap.get(key);
         }
         String newKey = key.substring(0, key.indexOf(separation));
         Object value = srcMap.get(newKey);
-        if (null == value) {
+        if (null == value)
+        {
             return "";
-        } else {
+        }
+        else
+        {
 
-            if (value instanceof Map) {
+            if (value instanceof Map)
+            {
                 Map<String, Object> newMap = (Map<String, Object>)value;
                 newKey = key.substring(key.indexOf(separation) + 1, key.length());
                 return getValue(newMap, newKey);
-            } else {
+            }
+            else
+            {
                 return value.toString();
             }
         }
